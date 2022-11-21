38e9009efeea5227ae057c1dcc4fea918e0cdadf
koren3210 <85394450+koren3210@users.noreply.github.com>
2022-11-21 18:23:57 +0200

    header-styled (#8)

---
+++ frontend/src/assets/styledComponents/base/Button.styled.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+type ButtonProps = {
+  width: string;
+  height: string;
+  fontSize: string;
+};
+
+export const PrimaryButton = styled.button<ButtonProps>`
+  background: ${({ theme }) => theme.colors.clr1};
+  color: ${({ theme }) => theme.colors.clr2};
+  border-radius: ${({ theme }) => theme.borders.borderRad1};
+  font-family: Poppins;
+  font-weight: 500;
+  transition: 0.3s ease;
+  width: ${(props) => (props.width ? props.width : "auto")};
+  height: ${(props) => (props.height ? props.height : "auto")};
+  font-size: ${(props) =>
+    props.fontSize === "s"
+      ? props.theme.fsS
+      : props.fontSize === "m"
+      ? props.theme.fsM
+      : props.theme.fsL};
+  font-weight: 500;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(110%);
+  }
+`;
---
+++ frontend/src/assets/styledComponents/components/FeaturedCategories.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FeaturedContainer = styled.div`
+  padding: 0 5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  margin-top: 72px;
+  margin-bottom: 100px;
+  gap: 27px;
+`;
