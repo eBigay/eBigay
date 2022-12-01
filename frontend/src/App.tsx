f196e06616b179074a10ae2d27b5a1bb05119dd6
kirilreznik <kirilreznik@gmail.com>
2022-12-01 18:17:20 +0200

    feat(modal): built generic modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,28 @@ import routes from "./routes";
 import Header from "./components/Header";
 import theme from "./theme";
 import Loading from "./components/Loading";
+import RootContextProvider from "./context/RootContext";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header />
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.key}
-                element={<route.element />}
-                path={route.path}
-              />
-            ))}
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </ThemeProvider>
+    <RootContextProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={route.key}
+                  element={<route.element />}
+                  path={route.path}
+                />
+              ))}
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </ThemeProvider>
+    </RootContextProvider>
   );
 };
 export default App;
