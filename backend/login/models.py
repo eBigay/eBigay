ca4ceebec4a98f500c704bbc566affe7b81ddf48
or haim perets <75416563+OR645@users.noreply.github.com>
2022-12-08 12:10:33 +0200

    new

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,19 +1,24 @@
-# from django.conf import settings
-# from django.db import models
+from django.conf import settings
+from django.db import models
 #
-#
-#
+
+
 # class DataBase(models.Model):
 #     name = models.CharField(max_length=100)
+#
 #     def __str__(self):
 #         return self.name
-#
-#
-# class Users(models.Model):
-#     username = models.ForeignKey(DataBase, on_delete=models.CASCADE)
-#     password = models.CharField(max_length=200)
-#     email = models.CharField(max_length=200)
-#
+
+
+class User(models.Model):
+    # username = models.ForeignKey(DataBase, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
 #
 # user = models.Users(username, password, email)
 # user.save()
