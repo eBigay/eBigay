4f8f72df28f867a80fcce30924c6939b74668a81
or haim perets <75416563+OR645@users.noreply.github.com>
2022-12-18 21:30:02 +0200

    /

--- backend/djangoProject1/settings.py
+++ backend/djangoProject1/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'orhaim645@gmail.com'
-EMAIL_HOST_PASSWORD = 'facawvtitutczmsa'
+EMAIL_HOST_USER = '<email_address>'
+EMAIL_HOST_PASSWORD = '<email_password>'
