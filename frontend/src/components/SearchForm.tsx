caebdc70aeac1ba552e8233ce67a325ee4a7d1a5
YanivWein24 <yanivwein22@gmail.com>
2023-01-20 17:29:19 +0200

    feat(autocomplete): moved the autocomplete to separate component

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,22 +1,16 @@
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
-import { UseQueryResult } from "react-query";
-import { TextField } from "@mui/material";
-import useItems from "../hooks/useItems";
 import PrimaryButton from "../assets/styles/base/Button.styled";
-import { IItem } from "../interfaces/IItem.interface";
-import { IFilterBy } from "../interfaces/IFilterBy.interface";
-import useDebounce from "../hooks/useDebounce";
 import {
   StyledButton,
   StyledClearIcon,
   StyledContainer,
   StyledForm,
   StyledFormContainer,
-  StyledAutocomplete,
   StyledSearchButton,
   StyledSearch,
 } from "../assets/styles/layout/SearchForm.styled";
+import Autocomplete from "./Autocomplete";
 
 interface ISearchFormProps {
   isSearchBarOpen: boolean;
@@ -27,44 +21,10 @@ const SearchForm = ({
   isSearchBarOpen,
   setIsSearchBarOpen,
 }: ISearchFormProps) => {
-  const inputSearchRef = useRef<HTMLInputElement>(null);
-
   const [query, setQuery] = useState<string>("");
-
-  const [filterBy, setFilterBy] = useState<IFilterBy>({
-    queryText: "",
-    category: "",
-    page: 0,
-    sortBy: "createdAt",
-    sortOrder: "desc",
-    limit: 6,
-  });
-
-  const { useQueryAllItems } = useItems();
-
-  const debouncedFilter = useDebounce(filterBy, 300);
-
-  const { data: results }: UseQueryResult<IItem[], any> =
-    useQueryAllItems(debouncedFilter);
-
-  const queryTextResults =
-    !results || results.length < 1 || !Array.isArray(results)
-      ? []
-      : results.map((result) => result.itemName);
 
   const navigate = useNavigate();
   const location = useLocation();
-
-  useEffect(() => {
-    if (inputSearchRef.current) {
-      inputSearchRef.current.focus();
-    }
-  }, []);
-
-  const handleChange = (value: string) => {
-    setQuery(value);
-    setFilterBy({ ...filterBy, queryText: value });
-  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -87,37 +47,7 @@ const SearchForm = ({
           <StyledSearchButton disabled={!query} type="submit">
             <StyledSearch isdisabled={query} />
           </StyledSearchButton>
-          <StyledAutocomplete
-            disablePortal
-            autoComplete
-            autoHighlight
-            freeSolo // allows to search when the options array is empty
-            id="autocomplete"
-            options={[...new Set(queryTextResults)]}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                key={params.id}
-                placeholder="Search here..."
-                sx={{
-                  "@media (max-width: 1000px)": {
-                    width: "100%",
-                  },
-                }}
-              />
-            )}
-            inputValue={query}
-            ref={inputSearchRef}
-            onInputChange={(event, value) => handleChange(value)}
-            sx={{
-              "@media (max-width: 1000px)": {
-                paddingTop: "5px",
-              },
-              "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-                border: "none",
-              },
-            }}
-          />
+          <Autocomplete query={query} setQuery={setQuery} />
           <PrimaryButton
             width="115px"
             height="50px"
