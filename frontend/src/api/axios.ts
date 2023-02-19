6cb5e575a2ca6bb7b12b7ff249f9ec62985a777c
Koren Turgeman <85394450+koren3210@users.noreply.github.com>
2023-02-19 13:42:31 +0200

    fix: backend (#56)
    * formik control
    
    * new item page
    
    * fix:eslint, temp errors
    
    * fix
    
    * react query update
    
    * fix:upload widget + profile page
    
    * backend-express
    
    * refactor:backend models + functionlaity
    
    * log folder
    
    * logs
    
    * log
    
    * fix: api

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "production" ? "/api/" : "http://localhost:3500";
+  import.meta.env.MODE === "production" ? "/" : "http://localhost:3500";
 
 export default axios.create({
   baseURL: BASE_URL,
