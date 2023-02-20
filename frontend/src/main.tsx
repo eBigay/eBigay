dae10e834d4936fd14730f41e7f2e5a169468e1b
Koren Turgeman <koren3210@gmail.com>
2023-02-20 11:20:30 +0200

    fix3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
-
-if (import.meta.env.PROD) disableReactDevTools();
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
